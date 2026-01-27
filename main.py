80708ee415a69f192514c255a0bffd345f51883f
Aryanr64x <aryansaket@yahoo.com>
2026-01-28 00:49:20 +0530

    updated cors

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["http://localhost:3000", 'https://smartorder-frontend-nine.vercel.app'],  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"])
